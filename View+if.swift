7a4c8f6d38b75fef797b53a66bef6be94619a5ea
Igor Zhukov <ihorzhukov@gmail.com>
2023-10-11 10:46:43 +0300

    documentation for View+if

--- View+if.swift
+++ View+if.swift
@@ -2,12 +2,24 @@ import Foundation
 import SwiftUI
 
 extension View {
+    
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`
     @ViewBuilder
     func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
         if condition { transform(self) }
         else { self }
     }
-
+    
+    /// Applies the given transform if the given condition evaluates to `true`, otherwise applies an alternate transform.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View` if the condition is `true`.
+    ///   - elseTransform: The transform to apply to the source `View` if the condition is `false`.
+    /// - Returns: Either the modified `View` using the `transform` if the condition is `true`, or the modified `View` using the `elseTransform` if the condition is `false`.
     @ViewBuilder
     func ifElse<Transform: View, ElseTransform: View>(_ condition: Bool, transform: (Self) -> Transform,
                                                       elseTransform: (Self) -> ElseTransform) -> some View {
